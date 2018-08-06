979aa4712bc05d8646f1037c8e80802e74cc5100
aytona <christopher.aytona@gmail.com>
2018-08-05 23:19:23 -0400

    Lab3

---
+++ Lab3/PrintManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab3 {
+    class PrintManager {
+        public String ID { get; set; }
+        private static int _idNum = 0;
+        private readonly int _id;
+        private static PrintManager[] _instance = null;
+
+        private PrintManager(int id) {
+            this.ID = "Sheridan Print Manager";
+            this._id = id;
+        }
+
+        public static PrintManager Instance {
+            get {
+                if (_instance == null) {
+                    _instance = new PrintManager[3];
+                }
+                int current = _idNum;
+                if (_instance[_idNum] == null) {
+                    _instance[_idNum] = new PrintManager(_idNum);
+                    if (_idNum + 1 < 3) {
+                        _idNum++;
+                    } else {
+                        _idNum = 0;
+                    }
+                    return _instance[current];
+                } else {
+                    bool empty = false;
+                    for (int i = 0; i < 3; ++i) {
+                        if (_instance[i] == null) {
+                            empty = true;
+                            current = i;
+                            break;
+                        }
+                    }
+                    if (empty) {
+                        _instance[current] = new PrintManager(current);
+                        _idNum = current;
+                        return _instance[current];
+                    } else {
+                        Random rnd = new Random((int)DateTime.UtcNow.Ticks);
+                        return _instance[rnd.Next(0, 2)];
+                    }
+                }
+            }
+        }
+
+        public String GetID() {
+            return "Print ID Number: " + this._id;
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3 {
+    class Program {
+        static void Main(string[] args) {
+            List<PrintManager> pmList = new List<PrintManager>();
+
+            PrintManager pm = PrintManager.Instance;
+            pm.ID = "My blah blah";
+            pmList.Add(pm);
+
+            PrintManager pm2 = PrintManager.Instance;
+            pm2.ID = "bleah bleah";
+            pmList.Add(pm2);
+
+            PrintManager pm3 = PrintManager.Instance;
+            pmList.Add(pm3);
+
+            PrintManager pm4 = PrintManager.Instance;
+            pmList.Add(pm4);
+
+            PrintManager pm5 = PrintManager.Instance;
+            pmList.Add(pm5);
+
+            foreach(PrintManager i in pmList) {
+                Console.WriteLine(i.GetID());
+                Console.WriteLine(i.ID);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
